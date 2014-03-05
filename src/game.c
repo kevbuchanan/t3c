dbfdae427e5c8dd8a93a6bf85006df8beed2a938
kevinbuch <kevaustinbuch@gmail.com>
2014-03-05 09:22:47 -0600

    refactored game loop

--- src/game.c
+++ src/game.c
@@ -5,13 +5,26 @@
 
 int do_turn(int turn, Board* board, Player* player, Player* other_player) {
   show_board(board);
+
   show_turn(turn + 1);
+
   int move = player->get_move(board, player->piece, other_player->piece);
+
   if (make_move(board, move, player->piece) != -1) {
     return 1 - turn;
   } else {
     show_invalid_move();
     return do_turn(turn, board, player, other_player);
+  }
+}
+
+void end_game(Board* board) {
+  show_board(board);
+
+  if (is_draw(board)) {
+    show_draw();
+  } else {
+    show_winner(winner(board));
   }
 }
 
@@ -26,11 +39,5 @@ void start_game(Board* board, Player* player1, Player* player2) {
     }
   }
 
-  show_board(board);
-
-  if (is_draw(board)) {
-    show_draw();
-  } else {
-    show_winner(winner(board));
-  }
+  end_game(board);
 }
