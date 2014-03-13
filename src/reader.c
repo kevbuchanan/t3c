85cbb86b1c31672bc66904ffd71caeb1a4a3b55b
kevinbuch <kevaustinbuch@gmail.com>
2014-03-13 12:54:22 -0500

    remove fpurge from reader

--- src/reader.c
+++ src/reader.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "reader.h"
 
+void flush() {
+  int c = 1;
+  while (c != '\n' && c != EOF) {
+    c = getchar();
+  }
+}
+
 int read_int() {
-  char input[3];
+  char input[4];
   int move = -1;
-  fgets(input, 3, stdin);
-  sscanf(input, "%d", &move);
-  fpurge(stdin);
+  fgets(input, 4, stdin);
+  if (strchr(input, '\n') == NULL) {
+    flush();
+  } else {
+    sscanf(input, "%d", &move);
+  }
   return move - 1;
 }
